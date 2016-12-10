a91517c166bfa965803b1c8a9c6c0a58e4c305b7
Harvnlenny <aaron@sparxoo.com>
2016-12-10 15:01:59 -0500

    setup

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,14 @@
+class Hamming
+
+  VERSION = 1
+   
+  def self.compute(a, b)
+    raise ArgumentError, "The two strands must have the same length." if a.length != b.length
+    (0..a.length).count do |i|
+      a[i] != b[i]
+    end
+      
+          
+  end
+
+end  
---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,23 @@
+class Complement
+
+  def self.of_dna(letter)
+      if letter [0] == 'C'
+        return "G"
+      elsif letter[0] == 'G'
+        return 'C'
+      elsif letter[0] == 'T'
+        return "A"
+      elsif letter[0] == "A"
+        return 'U'
+        
+        
+      end
+        
+    
+
+  
+      
+  end
+
+
+end
